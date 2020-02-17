d93b25c6e6ed402023191222f35860da0e8216ce
ZeeshanMalik0292 <43870064+ZeeshanMalik0292@users.noreply.github.com>
2020-02-17 14:47:04 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+class helloworld
+{
+//your program begins with call to main().
+//prints "Hello, World" to the terminal window.
+public static void main(String args[])
+{
+System.out.println("HelloWorld and welcome to java");
+System.out.println("we are checking whether java is installed or not");
+System.out.println("Developers will check the source code in this way");
+}
+}
+
